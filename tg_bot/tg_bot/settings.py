544e448f3ed7c4c2db28b3bd78a81c78120a7a76
andre <andre-ronzhin@yandex.ru>
2025-02-05 15:55:59 +0400

    fourth commit

--- tg_bot/tg_bot/settings.py
+++ tg_bot/tg_bot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14=523*z3=-6no_gn80a09m845jfxk2)a_%g8o90umtsf7eo0w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
