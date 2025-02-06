044ceb226f4f0c8b7c8169dfc66914c1be18b1b1
andre <andre-ronzhin@yandex.ru>
2025-02-06 12:28:46 +0400

    using a decorator

--- tg_bot/check_imei/server_tg_bot.py
+++ tg_bot/check_imei/server_tg_bot.py
@@ -16,26 +16,27 @@ def covert_response(values:dict[str, Any])->str:
     result = [f'{v}: {values.get(k)}' for k, v in names.items() if values.get(k, None)]
     return  '\n'.join(result)
 
-def check_user(user_tg):
+def user_is_allowed(user_tg):
     try:
         obj_user = UserTgBot.objects.get(user_id=user_tg.id)
     except ObjectDoesNotExist:
         obj_user = UserTgBot.objects.create(user_id=user_tg.id,
                                             name=user_tg.first_name,
                                             username=user_tg.username)
-
-    if not obj_user.access_is_allowed:
-        bot.send_message(user_tg.id, "Вы не зарегистрированы, дождитесь проверки администратора.")
-
     return obj_user.access_is_allowed
 
+def check_user(func):
+    def inner(message):
+        if user_is_allowed(message.from_user):
+            func(message)
+        else:
+            bot.send_message(message.from_user.id, "Вы не зарегистрированы, дождитесь проверки администратора.")
+    return inner
+
 @bot.message_handler(commands=['start'])
+@check_user
 def handle_start(message):
-    if not check_user(message.from_user):
-        return None
-
     bot.send_message(message.from_user.id, "Привет, введите IMEI устройства.")
-
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
@@ -50,10 +51,8 @@ def handle_help(message):
                                                       'Где "X" это число от 0 до 9 включительно')))
 
 @bot.message_handler(content_types=['text'])
+@check_user
 def handle_check_imei(message):
-    if not check_user(message.from_user):
-        return None
-
     imei = None
     try:
         imei = IMEI(message.text)
